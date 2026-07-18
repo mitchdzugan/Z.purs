c9675d966e8f5250340a616470dd3b988996da8a
Mitch Dzugan <mitchdzugan@gmail.com>
2026-07-18 11:19:47 -0500

    simplify

--- flake.nix
+++ flake.nix
@@ -2,23 +2,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    easy-purescript-nix.url = "github:justinwoo/easy-purescript-nix";
   };
 
-  outputs = { nixpkgs, flake-utils, easy-purescript-nix, ... }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        easy-ps = easy-purescript-nix.packages.${system};
       in
       {
         devShells = {
           default = pkgs.mkShell {
             name = "purescript-custom-shell";
             buildInputs = [
-              easy-ps.purs-0_15_15
-              easy-ps.purescript-language-server
-              easy-ps.purs-tidy
+              pkgs.purescript
               pkgs.nodejs_26
               pkgs.esbuild
             ];
