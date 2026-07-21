774f46d62bcb7ee3fcd03e5d9d493668fa75b2cc
Mitch Dzugan <mitchdzugan@gmail.com>
2026-07-21 12:14:10 -0500

    better logging

--- scripts/applyPsSuggest.js
+++ scripts/applyPsSuggest.js
@@ -6,8 +6,8 @@ const fProc = spawn("npx", ["ps-suggest", "--apply"]);
 bProc.stderr.pipe(fProc.stdin);
 bProc.stdout.pipe(fProc.stdin);
 
-bProc.stdout.on("data", (d) => console.log("build:i", d.toString().trim()));
-bProc.stderr.on("data", (d) => console.error("build:e", d.toString().trim()));
+bProc.stdout.on("data", (d) => console.log(d.toString().trim()));
+bProc.stderr.on("data", (d) => console.error(d.toString().trim()));
 
-fProc.stdout.on("data", (d) => console.log("fix:i", d.toString().trim()));
-fProc.stderr.on("data", (d) => console.error("fix:e", d.toString().trim()));
+fProc.stdout.on("data", (d) => console.log(d.toString().trim()));
+fProc.stderr.on("data", (d) => console.error(d.toString().trim()));
