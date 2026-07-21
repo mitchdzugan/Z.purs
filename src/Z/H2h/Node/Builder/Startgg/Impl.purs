f8d0bc4a8a4abd3e683e6b12b1c0db24f8aa242f
Mitch Dzugan <mitchdzugan@gmail.com>
2026-07-21 08:23:58 -0500

    checkin before ps-suggest

--- src/Z/H2h/Node/Builder/Startgg/Impl.purs
+++ src/Z/H2h/Node/Builder/Startgg/Impl.purs
@@ -2,21 +2,30 @@ module Z.H2h.Node.Builder.Startgg.Impl where
 
 import Prelude
 
+import Z as Z
+import Z.Gql.Node.Module as Gql
 import Z.H2h.Module as H2h
-import Z.H2h.Node.Builder.Startgg.Queries as Q
 import Z.H2h.Node.Builder.API as B
 import Z.H2h.Node.Builder.Startgg.All as All
-import Z as Z
+import Z.H2h.Node.Builder.Startgg.Queries as Q
+
+fetchRawEventData :: forall x. Z.X (B.BuildX x) Q.TourneyDataRes
+fetchRawEventData = do
+  fetchImpl Q.tourneyDataSmall (Z.xSet Gql._networkControl Gql.CacheOnly)
+  where
+  fetchImpl q plusEdit = do
+    { client, slug, optsEdit } <- Z.xAsk
+    let initVars = { pageE: 0, pageS: 0, slug }
+    let eSpec = All.ggPageSpec (Z.px @"pageE") (Z.ppx @"event" @"entrants")
+    let sSpec = All.ggPageSpec (Z.px @"pageS") (Z.ppx @"event" @"standings")
+    let pageSpecs = [ eSpec, sSpec ]
+    Z.xMapWE H2h.GqlW H2h.GqlE do
+      All.ggQueryAll q initVars pageSpecs client $ optsEdit *> plusEdit
 
 getEventData :: forall x. B.GetDataFn x
 getEventData = B.adaptBuilder do
-  { client, slug, optsEdit } <- Z.xAsk
-  let initVars = { pageE: 0, pageS: 0, slug }
-  let eSpec = All.ggPageSpec (Z.px @"pageE") (Z.ppx @"event" @"entrants")
-  let sSpec = All.ggPageSpec (Z.px @"pageS") (Z.ppx @"event" @"standings")
-  let pageSpecs = [ eSpec, sSpec ]
-  res <- Z.xMapWE H2h.GqlW H2h.GqlE do
-    All.ggQueryAll Q.tourneyData initVars pageSpecs client optsEdit
+  { slug } <- Z.xAsk
+  res <- fetchRawEventData
   Z.xInfo res
   pure
     { id: Z.asStringOr res.event.id
@@ -26,4 +35,3 @@ getEventData = B.adaptBuilder do
     , tournamentName: res.event.tournament.name
     , site: H2h.Startgg
     }
-
